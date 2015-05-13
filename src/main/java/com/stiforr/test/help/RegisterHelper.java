9e11c900a8c3bbbbc0be1b66ca7dabc497176f7b
Sean <norwood.sean@gmail.com>
2015-05-13 17:51:59 -0500

    Test

--- src/main/java/com/stiforr/test/help/RegisterHelper.java
+++ src/main/java/com/stiforr/test/help/RegisterHelper.java
@@ -13,6 +13,8 @@ public class RegisterHelper
 	{
 		GameRegistry.registerBlock(block, Reference.MODID + "_" + block.getUnlocalizedName().substring(5));
 		
+		GameRegistry.addSmelting(new ItemStack(Items.TritOre, 1, 1), new ItemStack(Items.TritIngot, 1, 1), 0.1F);
+		
 		
 	}
 	
